b2a0f2bb59ed9c9539ef0a87c8852c4477bfea8f
Ivan Yurchanka <vanya.yu@gmail.com>
2025-09-24 17:55:31 +0200

    Fix suppressions example

--- examples/suppressions_api.rb
+++ examples/suppressions_api.rb
@@ -13,22 +13,23 @@ suppressions = Mailtrap::SuppressionsAPI.new 3229, client
 list = suppressions.list
 # =>
 #  [
-#    Mailtrap::Suppression.new(
-#      id: "64d71bf3-1276-417b-86e1-8e66f138acfe",
-#      type: "unsubscription",
-#      created_at: "2024-12-26T09:40:44.161Z",
-#      email: "recipient@example.com",
-#      sending_stream: "transactional",
-#      domain_name: "sender.com",
-#      message_bounce_category: nil,
-#      message_category: "Welcome email",
-#      message_client_ip: "123.123.123.123",
-#      message_created_at: "2024-12-26T07:10:00.889Z",
-#      message_outgoing_ip: "1.1.1.1",
-#      message_recipient_mx_name: "Other Providers",
-#      message_sender_email: "hello@sender.com",
-#      message_subject: "Welcome!"
-#    )
+#    #<struct Mailtrap::Suppression
+#      id="64d71bf3-1276-417b-86e1-8e66f138acfe",
+#      type="unsubscription",
+#      created_at="2024-12-26T09:40:44.161Z",
+#      email="recipient@example.com",
+#      sending_stream="transactional",
+#      domain_name="example.com",
+#      message_bounce_category="",
+#      message_category="Welcome email",
+#      message_client_ip="123.123.123.123",
+#      message_created_at="2024-12-26T07:10:00.889Z",
+#      message_esp_response="",
+#      message_esp_server_type="",
+#      message_outgoing_ip="1.1.1.1",
+#      message_recipient_mx_name="Other Providers",
+#      message_sender_email="hello@sender.com",
+#      message_subject="Welcome!">
 #  ]
 
 # Delete a suppression
