1bd16fa5534822dd90c93b3363acbefbe4800469
DagonWat <yahor.vaitsiakhouski@railsware.com>
2026-01-05 12:53:00 +0100

    Fix rubocop issues

--- spec/mailtrap/sandbox_attachment_spec.rb
+++ spec/mailtrap/sandbox_attachment_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Mailtrap::SandboxAttachment do
         created_at: '2024-01-01T12:00:00Z',
         updated_at: '2024-01-02T12:00:00Z',
         attachment_human_size: '1 KB',
-        download_path: '/downloads/example.txt',
+        download_path: '/downloads/example.txt'
       }
     end
 
@@ -34,7 +34,7 @@ RSpec.describe Mailtrap::SandboxAttachment do
         created_at: '2024-01-01T12:00:00Z',
         updated_at: '2024-01-02T12:00:00Z',
         attachment_human_size: '1 KB',
-        download_path: '/downloads/example.txt',
+        download_path: '/downloads/example.txt'
       )
     end
   end
@@ -55,7 +55,7 @@ RSpec.describe Mailtrap::SandboxAttachment do
         created_at: '2024-01-01T12:00:00Z',
         updated_at: '2024-01-02T12:00:00Z',
         attachment_human_size: '1 KB',
-        download_path: '/downloads/example.txt',
+        download_path: '/downloads/example.txt'
       )
     end
 
@@ -72,7 +72,7 @@ RSpec.describe Mailtrap::SandboxAttachment do
         created_at: '2024-01-01T12:00:00Z',
         updated_at: '2024-01-02T12:00:00Z',
         attachment_human_size: '1 KB',
-        download_path: '/downloads/example.txt',
+        download_path: '/downloads/example.txt'
       )
     end
 
